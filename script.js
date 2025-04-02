1a8173b0186abf71258da7b722a69b644a65eb53
Phillip Roach <108208254+philliproach@users.noreply.github.com>
2025-04-01 21:55:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -35,10 +35,11 @@ document.addEventListener('DOMContentLoaded', function() {
         observer.observe(section);
     });
 
+document.addEventListener("DOMContentLoaded", function () {
     const menuButton = document.querySelector(".menu-button");
     const navMenu = document.querySelector("nav ul");
 
-    menuButton.addEventListener("click", function() {
+    menuButton.addEventListener("click", function () {
         navMenu.classList.toggle("show");
     });
 });
